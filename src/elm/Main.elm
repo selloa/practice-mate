d3a6aa590b5be322fa742d3cc9c95a2bb9a337b5
Felix Breitenbach <felix.breitenbach@gmail.com>
2020-05-25 20:26:46 +0200

    add timer

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,13 +1,24 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, br, button, div, p, text)
 import Html.Events exposing (onClick)
+import Time
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = \_ -> ( initialModel, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 type PracticeMode
-    = TimeLimit Int
-    | ExerciseLimit Int
+    = TimeLimit
+    | ExerciseLimit
 
 
 type Topic
@@ -37,49 +48,119 @@ type Pattern
     | Staccato Int
 
 
+
+-- Model
+
+
 type alias Model =
     { practiceMode : PracticeMode
     , topic : Topic
     , root : Root
     , range : Maybe Range
     , pattern : Maybe Pattern
+    , elapsedTime : Int
+    , completedExercises : Int
+    , isRunning : Bool
     }
 
 
 initialModel : Model
 initialModel =
-    { practiceMode = TimeLimit 900
+    { practiceMode = TimeLimit
     , topic = Scales
     , root = C
     , range = Nothing
     , pattern = Nothing
+    , elapsedTime = 0
+    , completedExercises = 0
+    , isRunning = False
     }
 
 
+
+-- update
+
+
 type Msg
-    = NoOp
+    = Tick Time.Posix
+    | ToggleTimer
+    | ClearTimer
+    | NoOp
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        Tick _ ->
+            if model.isRunning then
+                ( { model | elapsedTime = model.elapsedTime + 1 }, Cmd.none )
+
+            else
+                ( model, Cmd.none )
+
+        ToggleTimer ->
+            if model.isRunning then
+                ( { model | isRunning = False }, Cmd.none )
+
+            else
+                ( { model | isRunning = True }, Cmd.none )
+
+        ClearTimer ->
+            ( { model | isRunning = False, elapsedTime = 0 }, Cmd.none )
+
         NoOp ->
-            model
+            ( model, Cmd.none )
+
+
+
+-- subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Time.every 1000 Tick
+
+
+
+-- view
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick NoOp ] [ text "+1" ]
-        , button [ onClick NoOp ] [ text "-1" ]
-        , div [] [ text <| Debug.toString model ]
+        [ Html.h1 [] [ text "Rüben" ]
+        , header model
+        , p [] [ text <| Debug.toString model ]
         ]
 
 
-main : Program () Model Msg
-main =
-    Browser.sandbox
-        { init = initialModel
-        , view = view
-        , update = update
-        }
+header model =
+    let
+        minutes =
+            String.fromInt (model.elapsedTime // 60)
+
+        seconds =
+            String.fromInt (remainderBy 60 model.elapsedTime)
+
+        toDoubleDigits number =
+            if String.length number < 2 then
+                "0" ++ number
+
+            else
+                number
+    in
+    div []
+        [ text (toDoubleDigits minutes ++ ":" ++ toDoubleDigits seconds)
+        , br [] []
+        , text <| "finished exercises: " ++ String.fromInt model.completedExercises
+        , br [] []
+        , button [ onClick ToggleTimer ]
+            [ text <|
+                if model.isRunning then
+                    "▐▐ pause"
+
+                else
+                    "▶ ️ play"
+            ]
+        , button [ onClick ClearTimer ] [ text "■" ]
+        ]
